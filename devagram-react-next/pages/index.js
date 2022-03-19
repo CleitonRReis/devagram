026b66dfeb32454b1a8f50f759118b7779b66b23
Cleiton Reis <cleitonreis@MacBook-Pro-de-Cleiton.local>
2022-03-19 20:36:29 -0300

    Adicionando componente do avatar e sua estilização.

--- devagram-react-next/pages/index.js
+++ devagram-react-next/pages/index.js
@@ -1,11 +1,13 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Avatar from '../componentes/avatar'
 import Botao from '../componentes/botao'
 
 export default function Home() {
   return (
     <>
       <h1>Olá Mundo!</h1>
+      <Avatar/>
       <Botao texto={'Login'} cor='contorno' manipularClique={() => console.log('Botao clicado')} />
     </>
   )
