8e1087b2abba21a3911d0d0802ab60e7e597bef8
Cleiton Reis <cleitonreis@MacBook-Pro-de-Cleiton.local>
2022-03-19 20:21:07 -0300

    Estilizando botao login.

--- devagram-react-next/pages/index.js
+++ devagram-react-next/pages/index.js
@@ -1,8 +1,12 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Botao from '../componentes/botao'
 
 export default function Home() {
   return (
-    <h1>Olá Mundo!</h1>
+    <>
+      <h1>Olá Mundo!</h1>
+      <Botao texto={'Login'} cor='contorno' manipularClique={() => console.log('Botao clicado')} />
+    </>
   )
 }
